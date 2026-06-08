673778ff4ec04d73d7e30d596026bd8810e07b84
amanmaddheshiya2812 <amanmaddheshiya2812@gmail.com>
2026-06-08 15:41:31 +0530

    string

--- class_work/String 8 june/No of Charecter in Sentence.py
+++ class_work/String 8 june/No of Charecter in Sentence.py
@@ -1,9 +1,10 @@
-#WAP to input a string or a sentence from user and count the number of characters in the sentence and count the number of special characters in the sentence
+#WAP to input a string or a sentence from user and count the number of characters in the sentence and count the number of special characters in the sentence without using any built in function using len() function.
 string = input("Enter a string or sentence: ")
-char_count = len(string)
-special_count = 0
+char_count = 0
+special_char_count = 0
 for char in string:
-    if char in "!@#$%^&*( )_+-=[]{}|;':\",.<>?/":
-        special_count += 1
+    char_count += 1
+    if not char.isalnum() and not char.isspace():
+        special_char_count += 1
 print("Number of characters:", char_count)
-print("Number of special characters:", special_count)
+print("Number of special characters:", special_char_count)
